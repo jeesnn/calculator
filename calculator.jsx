45f023761cfa0f8dd16876838a90c45c1dbdec41
zhenjess <jessicazhen8@gmail.com>
2020-03-17 19:10:47 -0700

    Define add operation

--- calculator.jsx
+++ calculator.jsx
@@ -15,7 +15,7 @@ class Calculator extends React.Component {
         this.clear = this.clear.bind(this); //allow restart calculation default to sum 0
     }
 
-    setNum1(e) { //input event
+    setNum1(e) { //input event (Note: events are to be observed not mutated or intercepted)
         const num1 = e.target.value ? parseInt(e.target.value) : ""; //declare num1 to an integer, parse digits
         this.setState({ num1 }); //set state of num1
     } 
@@ -23,6 +23,14 @@ class Calculator extends React.Component {
     setNum2(e) {
         const num2 = e.target.value ? parseInt(e.target.value) : ""; //declare num2 to an integer, parse digits too
         this.setState({ num2 }); //set state of num2
+    }
+
+    //set state of each math operation on calculator
+    add(e) {
+        //prevents default action associated with the event
+        e.preventDefault(); //allows stop event (event's default side effect) from occurring 
+        const result = this.state.num1 + this.state.num2; //add operation of 2 nums
+        this.setState = ({ result }); //set state of add result for additional calculations
     }
 
     render() {
