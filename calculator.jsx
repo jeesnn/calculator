a4ec41ce21f9e7f1f97676b9ba87b5edf418caf6
zhenjess <jessicazhen8@gmail.com>
2020-03-17 18:46:40 -0700

    Set up format of calculator render component

--- calculator.jsx
+++ calculator.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class Calculator extends React.Component {
+    constructor() {
+        super();
+    }
 
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
 export default Calculator;
