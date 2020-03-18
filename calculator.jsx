7f022d421ee2b7cd6b8dd24e439aaf27052d2cc6
zhenjess <jessicazhen8@gmail.com>
2020-03-17 18:49:21 -0700

    Initialize and set state of num1 and num2

--- calculator.jsx
+++ calculator.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 class Calculator extends React.Component {
     constructor() {
         super();
+        this.state = { num1: "", num2: "", result: 0}; //initialize state to 2 nums and sum is 0
+        this.setNum1 = this.setNum1.bind(this); //initialize and bind num1 to instance of calculator to be used throughout the class component
+        this.setNum2 = this.setNum2.bind(this); //initialize and bind num2 to instance of calculator to be used throughout the class component
     }
 
     render() {
         return (
             <div>
-                
+
             </div>
         );
     }
